17c623a1a60a81319400b6504fec0fa9983afbf7
Alida Nola <alidanola@hotmail.com>
2025-10-18 01:46:22 -0500

    "talk" indicator for left component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,11 @@ export default function Home() {
       <div className="flex flex-col items-center group relative">
         <div className="flex items-center mb-8 relative">
           <div className="w-30 h-30 rounded-full bg-black transition-transform duration-300 group-hover:-translate-y-2" />
-          <div className="absolute left-full ml-3 top-1/2 -translate-y-1/2 w-12 h-1 bg-black opacity-0 group-hover:opacity-100 transition-opacity duration-300" />
+          <div className="absolute left-full ml-3 top-1/2 -translate-y-1/2 flex flex-col items-center gap-2 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+            <div className="w-3 h-1 bg-black -rotate-45" />
+            <div className="w-6 h-1 bg-black ml-6" />
+            <div className="w-3 h-1 bg-black rotate-45" />
+          </div>
         </div>
         <div className="w-40 h-40 bg-black transition-transform duration-300 group-hover:-translate-y-2" />
       </div>
